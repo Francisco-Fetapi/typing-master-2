11b6733cb156c8101178ca40ddff6f1d09696d18
= <=>
2022-08-19 20:50:47 -0300

    useInputText criado

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,4 +1,6 @@
 import { Box } from "@mui/material";
+import { useState } from "react";
+import useInputText from "../hooks/useInputText";
 import { InputTextContainer, Text, BoxColumnCenter } from "../styles/General";
 
 interface Props {
@@ -6,10 +8,11 @@ interface Props {
 }
 
 export default function InputText({ text }: Props) {
+  const { myText, type } = useInputText();
   return (
     <BoxColumnCenter>
       <InputTextContainer elevation={3}>
-        <Box mr={0.3}>Ola</Box>
+        <Box mr={0.3}>{myText}</Box>
       </InputTextContainer>
       <Box mt={1.2}>
         <Text color="textSecondary" variant="subtitle2">
