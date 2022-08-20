00f99d6ba4c67e2683b7ff47d66709dfe00c3c66
= <=>
2022-08-19 21:19:30 -0300

    useInputText - codigo inutilizado eliminado

--- src/hooks/useInputText.tsx
+++ src/hooks/useInputText.tsx
@@ -17,14 +17,8 @@ type InputEvent = React.ChangeEventHandler<HTMLInputElement> | undefined;
 
 export default function useInputText() {
   const [inputText, setInputText] = useState("");
-  // const type: EventType = (e) => {
-  //   if (notAllowedKeys.includes(e.key)) {
-  //     return;
-  //   }
-  //   setMyText((prev) => prev + e.key);
-  // };
+
   const type: InputEvent = (e) => {
-    console.log(inputText);
     setInputText(e.target.value);
   };
 
