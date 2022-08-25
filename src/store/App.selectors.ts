8f4e36d8edbdc645b9a1338186742a5b19254f53
= <=>
2022-08-25 12:17:20 +0100

    codigo inutilizado eliminado

--- src/store/App.selectors.ts
+++ src/store/App.selectors.ts
@@ -10,13 +10,10 @@ export const selectPhraseSize = (state: RootState) => {
   return Levels[state.app.currentLevel].numWords;
 };
 export const selectTextToTypeArray = (state: RootState) => {
-  // return transformTextToArray(state.app.textToType);
   return Levels[state.app.currentLevel].arrayText;
 };
 export const selectWordToType = (state: RootState) => {
-  // const words = transformTextToArray(state.app.textToType);
   const words = selectTextToTypeArray(state);
-  console.log(state.app.typedWords);
   return words[state.app.typedWords];
 };
 
