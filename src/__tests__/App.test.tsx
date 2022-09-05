455cfd556a1ed4919d94188d454b9bba5eff67ed
= <=>
2022-09-05 14:25:18 +0100

    ajustes

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,14 @@
+// Imports
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+
+// To Test
+import App from "../App";
+
+// Tests
+test("Renders main page correctly", async () => {
+  // Setup
+  render(<App />);
+  // Pre Expecations
+  expect(1 + 1).toBe(2);
+});
