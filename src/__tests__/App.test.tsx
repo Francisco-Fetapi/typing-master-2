20d938e15b0c0ba0eaad5aaab790e1083cefec4a
= <=>
2022-09-08 13:08:38 +0100

    removendo o render do APP no teste

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -8,7 +8,7 @@ import App from "../App";
 // Tests
 test("Renders main page correctly", async () => {
   // Setup
-  render(<App />);
+  // render(<App />);
   // Pre Expecations
   expect(1 + 1).toBe(2);
 });
