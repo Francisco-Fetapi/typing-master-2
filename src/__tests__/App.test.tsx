9f1f162ab93b4c714949d7d425b2366593ca4faf
= <=>
2022-09-09 07:59:36 +0100

    ajustes para configurar teste unitarios com jest

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -5,23 +5,7 @@ import user from "@testing-library/user-event";
 // To Test
 import App from "../App";
 
-// import ThemeProvider from "@mui/material/styles/ThemeProvider";
-// import  from "@mui/material/styles/createTheme";
-// import { Button, ThemeProvider, createTheme } from "@mui/material";
-
-// function Application() {
-//   const theme = createTheme({});
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Button>Clicar</Button>
-//     </ThemeProvider>
-//   );
-// }
-// Tests
 test("Renders main page correctly", async () => {
-  // Setup
-  // const app = render(<App />);
   render(<App />);
-  // Pre Expecations
-  expect(screen.getByText("Clicar")).toBeInTheDocument();
+  expect(screen.queryByText("Clicar")).toBeInTheDocument();
 });
