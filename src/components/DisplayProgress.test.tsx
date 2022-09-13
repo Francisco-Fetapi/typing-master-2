5f27f2129a86d424e7275a70cb83556c460ffbf4
= <=>
2022-09-13 10:26:54 +0100

    Testando o DisplayProgress

--- src/components/DisplayProgress.test.tsx
+++ src/components/DisplayProgress.test.tsx
@@ -1,19 +1,9 @@
 // Imports
-import { render, screen } from "@testing-library/react";
-// import App from "../App";
-// import user from "@testing-library/user-event";
+import { render } from "@testing-library/react";
 import { AppSetup } from "../test";
 import DisplayProgress from "./DisplayProgress";
 
-// test("Renders main page correctly", async () => {
-//   render(<App />);
-//   // expect(screen.queryByText("Typing Master")).toBeInTheDocument();
-//   const btnStart = screen.getByTestId("btn-Iniciar");
-
-//   await user.click(btnStart);
-//   expect(screen.queryByText("Typing Master")).not.toBeInTheDocument();
-// });
-test("Renders main page correctly", async () => {
+test("Renders DisplayProgress component correctly", async () => {
   render(
     <AppSetup>
       <DisplayProgress />
