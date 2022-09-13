bea5d46495333638361daa93db738b6c42493e03
= <=>
2022-09-13 10:51:13 +0100

    configurar o store agnostico ao ambiente

--- src/store/App.store.ts
+++ src/store/App.store.ts
@@ -39,7 +39,7 @@ const initialState: App = {
   phraseTraining: selectRandomElement<TrainingPhrase>(trainingPhrases),
 };
 
-function stateReseted(): App {
+function stateReseted(initialState: App): App {
   const darkMode = useStatePersist<boolean>(THEME_KEY_IN_LOCALSTORAGE).get();
   const currentLevel =
     useStatePersist<number>(CURRENT_LEVEL_KEY_IN_LOCALSTORAGE).get() || 0;
@@ -48,72 +48,79 @@ function stateReseted(): App {
   return { ...initialState, darkMode, currentLevel, points, phraseTraining };
 }
 
-export const app = createSlice({
-  name: "app",
-  initialState,
-  reducers: {
-    toggleTheme(state) {
-      state.darkMode = !state.darkMode;
-      const { save } = useStatePersist<boolean>(THEME_KEY_IN_LOCALSTORAGE);
-      save(state.darkMode);
+function sliceCreator(initialState: App) {
+  return createSlice({
+    name: "app",
+    initialState,
+    reducers: {
+      toggleTheme(state) {
+        state.darkMode = !state.darkMode;
+        const { save } = useStatePersist<boolean>(THEME_KEY_IN_LOCALSTORAGE);
+        save(state.darkMode);
+      },
+      increaseTypedWords(state) {
+        state.typedWords = state.typedWords + 1;
+      },
+      clearTypedWords(state) {
+        state.typedWords = 0;
+      },
+      increaseLevel(state) {
+        state.currentLevel = Math.min(
+          state.currentLevel + 1,
+          Levels.length - 1
+        );
+        Object.assign(state, stateReseted(initialState));
+      },
+      showMessageBackdrop(state, action: PayloadAction<Backdrop.Props>) {
+        state.backdrop.open = true;
+        Object.assign(state.backdrop, action.payload);
+      },
+      hideMessageBackdrop(state) {
+        state.backdrop.open = false;
+      },
+      resetAllState(state) {
+        Object.assign(state, stateReseted(initialState));
+      },
+      setTimer(state, action: PayloadAction<number | null>) {
+        state.timer = action.payload;
+      },
+      playTimer(state) {
+        state.timerPaused = false;
+      },
+      pauseTimer(state) {
+        state.timerPaused = true;
+      },
+      increasePoints(state, action: PayloadAction<number>) {
+        state.points += action.payload;
+        const { save } = useStatePersist<number>(POINTS_KEY_IN_LOCALSTORAGE);
+        save(state.points);
+      },
+      chooseRandomPhraseToTrain(state) {
+        state.phraseTraining =
+          selectRandomElement<TrainingPhrase>(trainingPhrases);
+      },
     },
-    increaseTypedWords(state) {
-      state.typedWords = state.typedWords + 1;
-    },
-    clearTypedWords(state) {
-      state.typedWords = 0;
-    },
-    increaseLevel(state) {
-      state.currentLevel = Math.min(state.currentLevel + 1, Levels.length - 1);
-      Object.assign(state, stateReseted());
-    },
-    showMessageBackdrop(state, action: PayloadAction<Backdrop.Props>) {
-      state.backdrop.open = true;
-      Object.assign(state.backdrop, action.payload);
-    },
-    hideMessageBackdrop(state) {
-      state.backdrop.open = false;
-    },
-    resetAllState(state) {
-      Object.assign(state, stateReseted());
-    },
-    setTimer(state, action: PayloadAction<number | null>) {
-      state.timer = action.payload;
-    },
-    playTimer(state) {
-      state.timerPaused = false;
-    },
-    pauseTimer(state) {
-      state.timerPaused = true;
-    },
-    increasePoints(state, action: PayloadAction<number>) {
-      state.points += action.payload;
-      const { save } = useStatePersist<number>(POINTS_KEY_IN_LOCALSTORAGE);
-      save(state.points);
-    },
-    chooseRandomPhraseToTrain(state) {
-      state.phraseTraining =
-        selectRandomElement<TrainingPhrase>(trainingPhrases);
-    },
-  },
-});
+  });
+}
 
+export const app = sliceCreator(initialState);
+
+export const middlewares = {
+  serializableCheck: {
+    // Ignore these paths in the state
+    ignoredPaths: [
+      "app.phraseTraining",
+      "app.backdrop.primaryButton.handleClick",
+      "app.backdrop.onMount",
+      "payload.primaryButton.handleClick",
+    ],
+  },
+};
 export const store = configureStore({
   reducer: {
     app: app.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Ignore these paths in the state
-        ignoredPaths: [
-          "app.phraseTraining",
-          "app.backdrop.primaryButton.handleClick",
-          "app.backdrop.onMount",
-          "payload.primaryButton.handleClick",
-        ],
-      },
-    }),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(middlewares),
 });
 
 export const {
