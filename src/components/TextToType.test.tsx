3af81f36c20590fb52dea814a44090cf82a208c3
= <=>
2022-09-14 12:14:59 +0100

    Testes no componente TextToType

--- src/components/TextToType.test.tsx
+++ src/components/TextToType.test.tsx
@@ -1,6 +1,5 @@
 // Imports
 import { render } from "@testing-library/react";
-import user from "@testing-library/user-event";
 import { AppSetup } from "../test";
 import TextToType from "./TextToType";
 
