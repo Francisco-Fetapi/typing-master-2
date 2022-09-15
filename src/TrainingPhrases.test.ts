49dadb22996450746545824f0f0d73bc83edd0d6
= <=>
2022-09-15 08:01:19 +0100

    teste da funcao para definir o nivel de uma frase no treino criada

--- src/TrainingPhrases.test.ts
+++ src/TrainingPhrases.test.ts
@@ -1,9 +1,22 @@
 import { TrainingPhrase } from "./TrainingPhrases";
 
-const sut = new TrainingPhrase("Ola Mundo");
+const sut = new TrainingPhrase("Ola Mundo.");
 
 describe("TrainingPhrase", () => {
   it("should have time equal zero", () => {
     expect(sut.timeLimit).toBe(0);
+  });
+  it("should define a level by words number and time for typing", () => {
+    expect(sut.defineLevelByTimeAndNumLetters(6)).toBe("Beginner");
+    expect(sut.defineLevelByTimeAndNumLetters(7)).toBe("Beginner");
+    expect(sut.defineLevelByTimeAndNumLetters(11)).toBe("Beginner");
+
+    expect(sut.defineLevelByTimeAndNumLetters(5)).toBe("Intermediate");
+    expect(sut.defineLevelByTimeAndNumLetters(4)).toBe("Intermediate");
+    expect(sut.defineLevelByTimeAndNumLetters(3)).toBe("Intermediate");
+
+    expect(sut.defineLevelByTimeAndNumLetters(1)).toBe("Advanced");
+    expect(sut.defineLevelByTimeAndNumLetters(2)).toBe("Advanced");
+    expect(sut.defineLevelByTimeAndNumLetters(0.9)).toBe("Advanced");
   });
 });
