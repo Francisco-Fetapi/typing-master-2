d727bcabe6ddd48baf0685ab33188b0364d2d5ef
= <=>
2022-09-16 10:00:50 +0100

    Tabela com as classificacoes criada

--- src/components/ModalTrainingResult.tsx
+++ src/components/ModalTrainingResult.tsx
@@ -7,10 +7,19 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, TableCell, TableRow } from "@mui/material";
 import { Text } from "../styles/General";
-import { useNavigate } from "react-router-dom";
 import { TrainingContext } from "../contexts/TrainingContextProvider";
+import TableSimple from "./TableSimple";
+import { useSelector } from "react-redux";
+import { ILevel } from "../Levels";
+import {
+  selectCurrentLevelInfo,
+  selectPhraseTraining,
+  selectTimer,
+} from "../store/App.selectors";
+import { TrainingPhrase } from "../TrainingPhrases";
+import { timeTransformer2 } from "../helpers/timeTransformer";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -21,8 +30,47 @@ const Transition = React.forwardRef(function Transition(
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
+const rowsLevel = Object.keys(TrainingPhrase.levelRoles) as ILevel[];
+
+// const RowTitle = (
+//   <React.Fragment>
+//     {rowsLevel.map((row, key) => (
+//       <TableCell align={key === 0 ? undefined : "right"}>{row}</TableCell>
+//     ))}
+//   </React.Fragment>
+// );
+
+const RowTitle = (
+  <React.Fragment>
+    <TableCell>Nivel</TableCell>
+    <TableCell align="right">Intervalo</TableCell>
+  </React.Fragment>
+);
+
 export default function ModalTrainingResult() {
   const { open, handleClose } = useContext(TrainingContext);
+  const time = useSelector(selectTimer);
+  const phrase = useSelector(selectPhraseTraining);
+  const intervalTimePerLevel = phrase.showIntervalTimeByLevel();
+
+  const RowData = (
+    <React.Fragment>
+      {rowsLevel.map((role, key) => {
+        const level = intervalTimePerLevel[role]!;
+        return (
+          <TableRow>
+            <TableCell component="th" scope="row">
+              {role}
+            </TableCell>
+            <TableCell align="right">
+              {timeTransformer2(level.from)} - {timeTransformer2(level.to)}
+            </TableCell>
+          </TableRow>
+        );
+      })}
+    </React.Fragment>
+  );
+
   return (
     <div>
       <Dialog
@@ -53,6 +101,10 @@ export default function ModalTrainingResult() {
             habilidades na arte da digitação.
           </Text>
           <Box sx={{ zoom: 0.9 }}>Ola Mundo. Conteudo.</Box>
+
+          <Box mt={2}>
+            <TableSimple headTitle={RowTitle} rows={RowData} />
+          </Box>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Fechar</Button>
