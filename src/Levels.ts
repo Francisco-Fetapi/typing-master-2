36949e695469384d60ea7942d5963c474fad180a
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-09-20 12:38:31 +0100

    definir um limite de frases do jogo

--- src/Levels.ts
+++ src/Levels.ts
@@ -2,10 +2,11 @@ import { ILevel, Level } from "./Level";
 import phrases from "./phrases.json";
 
 const Levels: Level[] = [];
+const limitPhrases = 15;
 let role: ILevel;
 
 for (role in Level.levelRoles) {
-  phrases.forEach((phrase) => {
+  phrases.slice(0, limitPhrases).forEach((phrase) => {
     Levels.push(new Level(phrase, role));
   });
 }
