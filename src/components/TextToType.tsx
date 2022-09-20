ac7f01a6132355dfd0c084399a6e86f160f119a0
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-09-20 14:04:35 +0100

    redefinindo alguns estilos

--- src/components/TextToType.tsx
+++ src/components/TextToType.tsx
@@ -22,12 +22,12 @@ export default function TextToType() {
         <Text
           component="span"
           key={key}
-          // color={key < typedWords ? "blue" : "inherit"}
           sx={(theme) =>
             key < typedWords
               ? {
                   borderBottom: `1px dashed ${theme.palette.primary.main}`,
                   color: theme.palette.primary.main,
+                  fontWeight: `400 !important`,
                 }
               : {}
           }
